a63ed69f60281a0d5d03bd35bcfff5d209851970
Student <student@ent-v321-003.local>
2017-09-26 17:43:49 -0400

    arduino 1

---
+++ Day 8/sketch_sep26a/sketch_sep26a.ino
@@ -0,0 +1,22 @@
+int counter=0;
+int buttonPin=8;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+
+}
+
+void loop() {
+
+  int buttonState = digitalRead(buttonPin);
+ 
+  
+  
+if (buttonState==0) {
+  Serial.println(counter++);
+}
+
+  
+}
