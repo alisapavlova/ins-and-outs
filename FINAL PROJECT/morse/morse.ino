ce59b2ac6d6e2beeced83249cfde21f808759510
alisapavlova <student@ent-v321-003.local>
2017-12-05 17:49:40 -0500

    arduino morse

--- FINAL PROJECT/morse/morse.ino
+++ FINAL PROJECT/morse/morse.ino
@@ -4,10 +4,15 @@
 Servo y;
 int servoYpin = 9;
 
-
+int dotDelay = 110;
+int dashDelay = dotDelay*3;
+int betweenCharDelay = dotDelay*3;
+int betweenWords = dotDelay*6;
+int down = 43;
+int up = 65;
 
 // variables to store serial info
-int valY = 90;
+int valY = up;
 
 
 void setup() {
@@ -24,16 +29,173 @@ void setup() {
 void loop() {
 
 
-  if (Serial.available() > 1) {
+  if (Serial.available() > 0) {
 
-    // read the incoming bytes
-    valY = Serial.read();
-    
+    String str = Serial.readStringUntil('\n');
+
+    Serial.println(str);
+
+    for (int i = 0; i < str.length(); i++) {
+      char myChar = str.charAt(i);
+
+      if (myChar == 'a') {
+        makeDot();
+        makeDash();
+      }
+      if (myChar == 'b') {
+        makeDash();
+        makeDot();
+        makeDot();
+        makeDot();
+      }
+      if (myChar == 'c') {
+        makeDash();
+        makeDot();
+        makeDash();
+        makeDot();
+      }
+      if (myChar == 'd') {
+        makeDash();
+        makeDot();
+        makeDot();
+      }
+      if (myChar == 'e') {
+        makeDash();
+      }
+      if (myChar == 'f') {
+        makeDot();
+        makeDot();
+        makeDash();
+        makeDot();
+      }
+      if (myChar == 'g') {
+        makeDash();
+        makeDash();
+        makeDot();
+      }
+      if (myChar == 'h') {
+        makeDot();
+        makeDot();
+        makeDot();
+        makeDot();
+      }
+      if (myChar == 'i') {
+        makeDot();
+        makeDot();
+      }
+      if (myChar == 'j') {
+        makeDot();
+        makeDash();
+        makeDash();
+        makeDash();
+      }
+      if (myChar == 'k') {
+        makeDash();
+        makeDot();
+        makeDash();
+      }
+      if (myChar == 'l') {
+        makeDot();
+        makeDash();
+        makeDot();
+        makeDot();
+      }
+      if (myChar == 'm') {
+        makeDash();
+        makeDash();
+      }
+      if (myChar == 'n') {
+        makeDash();
+        makeDot();
+      }
+      if (myChar == 'o') {
+        makeDash();
+        makeDash();
+        makeDash();
+      }
+      if (myChar == 'p') {
+        makeDot();
+        makeDash();
+        makeDash();
+        makeDot();
+      }
+      if (myChar == 'q') {
+        makeDash();
+        makeDash();
+        makeDot();
+        makeDash();
+      }
+      if (myChar == 'r') {
+        makeDot();
+        makeDash();
+        makeDot();
+      }
+      if (myChar == 's') {
+        makeDot();
+        makeDot();
+        makeDot();
+      }
+        if (myChar == 't') {
+        makeDash();
+      }
+      if (myChar == 'u') {
+        makeDot();
+        makeDot();
+        makeDash();
+      }
+      if (myChar == 'v') {
+        makeDot();
+        makeDot();
+        makeDot();
+        makeDash();
+      }
+      if (myChar == 'w') {
+        makeDot();
+        makeDash();
+        makeDash();
+      }
+      if (myChar == 'x') {
+        makeDash();
+        makeDot();
+        makeDot();
+        makeDash();
+      }
+      if (myChar == 'y') {
+        makeDash();
+        makeDot();
+        makeDash();
+        makeDash();
+      }
+      if (myChar == 'z') {
+        makeDash();
+        makeDash();
+        makeDot(); 
+        makeDot();       
+      }
+      if (myChar == ' ') {
+        delay(betweenWords);    
+      }
+
+
+
+      delay(betweenCharDelay);
+
+    }
   }
+}
 
-  // rotate the servo
-  y.write(valY);
+void makeDot() {
+  y.write(down);
+  delay(dotDelay);
+  y.write(up);
+  delay(dotDelay);
+}
 
-  delay(2);
+void makeDash() {
+  y.write(down);
+  delay(dashDelay);
+  y.write(up);
+  delay(dotDelay);
+
 }
 
