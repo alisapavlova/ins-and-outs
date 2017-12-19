1556ff2965278f8235481eb318a4dbbce9e098d9
alisapavlova <31454550+alisapavlova@users.noreply.github.com>
2017-12-19 16:11:45 -0500

    Update Speaker_button.ino

--- FINAL PROJECT/Speaker_button/Speaker_button.ino
+++ FINAL PROJECT/Speaker_button/Speaker_button.ino
@@ -1,21 +1,26 @@
-int speakerPin = 6;
+int speakerPin = 6; // digital pin to attatch speaker
 
-int yPin = A0;
-int buttonPin = 7;
-int yPosition = 0;
+int yPin = A0;  // analog joystick pin 
+int buttonPin = 7; // digital joystick pin
+int yPosition = 0;  // position of the joystick
+
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600); 
   
-  pinMode(yPin, INPUT);
+  pinMode(yPin, INPUT); // set pin as an input
 
-  pinMode (speakerPin, OUTPUT);
+  pinMode (speakerPin, OUTPUT); // set pin as an output
+
 }
 
 void loop() {
 
-  yPosition = analogRead(yPin);
-Serial.println(yPosition);
+  yPosition = analogRead(yPin); // read the value from the joystick position
+  
+  Serial.println(yPosition);
+  
+  // when the position value is less than 200, meaning when the joystick is out of home position => make speaker beep
   if (yPosition < 200) {
     digitalWrite(speakerPin, HIGH);
     delayMicroseconds(100);
